159be1cf0de757b134812bec1b61e5498061e54d
Paul Franklin Wilkerson III <37197219+Paul-W-0@users.noreply.github.com>
2018-10-07 15:54:07 -0500

    Add files via upload
    Added the main C# file to the master branch.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace userstorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string>names = new List<string> {  };
+            Console.WriteLine("User Names (seperated by commas w/ space): ");
+            string input_names = Console.ReadLine();
+            names.Add(input_names);
+            if (input_names.Contains(",")) // If input contains multiple users: 
+            {  
+                names = input_names.Split(", ").ToList();
+                Console.WriteLine("Who are you looking for: ");
+                string search = Console.ReadLine();
+                 if (names.Contains(search)) // If the user specified does exist: 
+                {
+                    Console.WriteLine("Yes, the user exists....");
+                }
+                 else // If the user specified does not exist: 
+                {
+                    Console.WriteLine("No such user in the current directory....");
+                }
+            }
+            else // If the list only contains one user: 
+            {
+                Console.Write(names);
+            }
+        }
+    }
+}
